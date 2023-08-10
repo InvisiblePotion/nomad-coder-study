dd5dc6fc456e195068497c883a7e26edfc89d3e1
InvisiblePotion <invisibleppls@gmail.com>
2023-08-10 18:34:35 +0900

    4챕터 interface에 대한 내용 보강

--- typescript-for-beginners/ts/interface.ts
+++ typescript-for-beginners/ts/interface.ts
@@ -2,22 +2,38 @@ interface SStorage<T> {
     [key: string]: T;
 }
 
+// 키 밸류 형식의 저장소 클래스 구현
 class LocalStorage<T> {
+    // 데이터가 저장되는 필드
     private storage: SStorage<T> = {};
+
+    // 데이터 삽입 함수
     set(key: string, value: T) {
+        // 해당 키가 이미 존재한다면 데이터 삽입 거부
         if (this?.storage[key]) {
-            console.log(`"${key}"이(가) 이미 존재합니다.`);
-            return
+            console.log(`"${key}"이(가) 이미 존재합니다.\n데이터 변경을 위해서는 먼저 remove()로 삭제해 주시기 바랍니다.`);
+            return;
         }
         this.storage[key] = value;
     }
+    // 데이터 삭제 함수
     remove(key: string) {
-        delete this.storage[key]
+        delete this.storage[key];
     }
-    // get(key: string): T {}
+    // 데이터 호출 함수
+    get(key: string): T {
+        return this.storage[key];
+    }
+    // 데이터 전체 삭제 함수
+    clear() {
+        this.storage = {};
+    }
 }
 
-const storage: LocalStorage<string> = new LocalStorage;
-storage.set("abc", "qwer");
+// 테스트
+const stringStorage = new LocalStorage<string>();
+stringStorage.set("abc", "qwer");
 
-console.log("end");
+const booleanStorage = new LocalStorage<boolean>();
+
+console.log("EOF");
