0dbc903781c2ac5a9706f539eb7a3d44a3e7dc94
InvisiblePotion <invisibleppls@gmail.com>
2023-08-18 16:39:04 +0900

    7.3 완료

--- react-for-beginners/react-app/src/index.js
+++ react-for-beginners/react-app/src/index.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import CoinTracker from "./CoinTracker"
+import MovieApp from "./MovieApp";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div>
-    <CoinTracker />
+    <MovieApp />
   </div>
 );
-/* 6챕터에서 작성된 연습용 코드를 사용하려면 이 친구들 사용
-  import UseEffectExample from "./useEffectExample";
-  import ButtonExample from './cssModuleExample';
-  <UseEffectExample />
-  <ButtonExample text='Button' />
-*/
