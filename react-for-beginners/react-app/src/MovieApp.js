86d1d23d8f869fcd9ed9c6f26adf99bafe978d65
InvisiblePotion <invisibleppls@gmail.com>
2023-08-20 23:35:14 +0900

    7.5 완료

--- react-for-beginners/react-app/src/MovieApp.js
+++ react-for-beginners/react-app/src/MovieApp.js
@@ -1,5 +1,22 @@
+import { 
+  BrowserRouter as Router,
+  Switch,
+  Route,
+ } from "react-router-dom";
+ import Home from "./routes/Home";
+ import Detail from "./routes/Detail";
+
 function MovieApp() {
-  return null;
+  return <Router>
+    <Switch>
+      <Route path="/">
+        <Home />
+      </Route>
+      <Route path="/movie">
+        <Detail />
+      </Route>
+    </Switch>
+  </Router>;
 }
 
 export default MovieApp;
--- react-for-beginners/react-app/src/routes/Detail.js
+++ react-for-beginners/react-app/src/routes/Detail.js
@@ -1,4 +1,11 @@
+import { Link } from "react-router-dom";
+
 function Detail() {
-  return <h1>Detail</h1>;
+  return (
+    <div>
+      <h1>Detail</h1>
+      <Link to="/">return</Link>
+    </div>
+  );
 }
 export default Detail;
