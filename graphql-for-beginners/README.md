5a415742e7f73b414844143eae2f8d10ce2e6b70
InvisiblePotion <invisibleppls@gmail.com>
2023-08-23 10:12:06 +0900

    4.6 ~ 4.7 완료

--- graphql-for-beginners/README.md
+++ graphql-for-beginners/README.md
@@ -3,13 +3,26 @@
 
 2023-07-07 ~ ongoing
 
-## 메모
+## 프로젝트 특이사항
 ### 설치된 패키지
 * dependencies
     + `apollo-server: 3.12.0`
     + `graphql: 16.7.1`
 * devDependencies
     + `nodemon: 3.0.0`
+---
+### 기타
+* 본 프로젝트에서는 Apollo GraphQL의 apollo-server 패키지를 사용하여 GraphQL 서버를 만들고 있다.
+
+## 메모
+### Apollo Server
+* SDL 작성 및 사용
+    + `apollo-server` 패키지에서 `gql` 함수를 import 한 다음 ```gql(`백틱 내부에 SDL 작성`)``` 와 같이 SDL을 작성한다.
+    + SDL이 작성된 `gql()` 함수의 반환 값을 `ApolloServer` 클래스의 매개변수인 `config`의 `typeDefs` 요소에 입력하여 사용한다.
+* Resolver 작성 및 사용
+    + 객체 안에 Resolver가 적용 될 타입들을 키로 하여 값으로 타입의 각 필드를 함수명으로 하는 함수들을 가진 객체를 입력한다.
+        - 예시: ```{ Query: { field1() { return "massage" } } }```
+    + 해당 객체를 `ApolloServer` 클래스 매개변수인 `config`의 `resolvers` 요소에 입력하여 사용한다.
 ---
 ### SDL: Schema Definition Language
 * SDL은 어떤 데이터가 어떻게 구조화 되어 있는지, 어떤 타입들이 존재하는지, 어떤 쿼리와 뮤테이션을 지원하는지를 정의하는 스키마를 정의하는 언어다.
@@ -22,3 +35,10 @@
     + `[String]!`은 이 필드가 반드시 배열을 포함해야 하며 해당 배열에는 Nullable인 `String`이 들어갈 수 있다는 뜻이다.
     + `[String!]`은 이 필드가 Nullable인 배열을 포함하며 해당 배열에는 반드시 `String`이 들어가야 한다는 뜻이다.
     + `[String!]!`은 이 필드가 반드시 배열을 포함해야 하며 해당 배열에는 반드시 `String`이 들어가야 한다는 뜻이다.
+---
+### Resolver
+* Resolver란 SDL에 명시한 필드에 대한 요청이 들어온 경우 GraphQL 서버가 실제로 실행시키게 되는 함수를 말한다.
+* SDL이 필드명과 타입, Null 여부에 대한 제약조건을 걸어줬다면 Resolver는 해당 제약조건에 부합하는 반환 값을 가진 함수를 작성하여 요청에 대해 필요한 로직을 작성할 수 있다.
+    + 예를 들어 `Query` 타입의 `banana`라는 필드에 대한 요청에 대해 `"Yellow"`라는 답을 하고 싶다면 Resolver 객체의 `Query` 요소에 `{ banana() { return "Yellow" } }` 와 같이 필드의 이름을 함수 명으로 갖는 함수가 들어간 객체를 입력해주면 된다.
+    + 실제 사용시에는 DB가 연결되어 있을테니 함수 내부에서 SQL을 실행하여 필요한 정보를 DB로부터 받아오거나 DB를 수정하게 될 것이다.
+    + 원칙 상 DB의 수정이 가능한 `Mutation`과 달리 `Query` 객체의 Resolver는 데이터 제공만을 해야하겠지만 작동 원리 상 `Query` 객체의 Resolver를 통해 DB의 수정이 이루어지더라도 GraphQL 서버는 정상 작동한다. `Query`와 `Mutation`의 분리는 약속에 불과하며 때문에 이를 지키기 위해 Resolver 함수 작성에 주의가 필요하다.
--- graphql-for-beginners/server.js
+++ graphql-for-beginners/server.js
@@ -1,48 +1,90 @@
+// NodeJS를 통해 구동되는 서버 클래스인 ApolloServer와 SDL 작성을 위한 gql 함수를 import
 import { ApolloServer, gql } from "apollo-server";
 
+// 임시 모조 데이터베이스
+let tweets = [
+  {
+    id: "1",
+    text: "first one",
+  },
+  {
+    id: "2",
+    text: "second one",
+  },
+];
+
 /*
-    SDL: Schema Definition Language
+  SDL: Schema Definition Language
 
-    SDL에는 GraphQL API에서 사용하는 타입을 설명해주어야 한다.
-    SDL의 type은 자바의 class와 유사하여 내부에 field를 생성할 수 있다.
-    field는 반드시 type을 가지며 유저가 만든 type을 가지거나 배열의 형태로 가지는 것 또한 가능하다.
-    
-    Query는 필수 설명 타입이며 정보의 조회를 위한 설명 타입이다.
-    Mutation은 백엔드(DB)에 변화를 주는 요청을 위한 설명 타입이다.
-    즉, Query는 REST의 GET 요청에, Mutation은 다른 나머지 요청에 해당한다고 볼 수 있다.
+  SDL에는 GraphQL API에서 사용하는 타입을 설명해주어야 한다.
+  SDL의 type은 자바의 class와 유사하여 내부에 field를 생성할 수 있다.
+  field는 반드시 type을 가지며 유저가 만든 type을 가지거나 배열의 형태로 가지는 것 또한 가능하다.
+  
+  Query는 필수 설명 타입이며 정보의 조회를 위한 설명 타입이다.
+  Mutation은 백엔드(DB)에 변화를 주는 요청을 위한 설명 타입이다.
+  즉, Query는 REST의 GET 요청에, Mutation은 다른 나머지 요청에 해당한다고 볼 수 있다.
 
-    타입의 뒤에 '!'를 붙이면 해당 필드에 Non-Null 속성을 적용할 수 있다.
+  타입의 뒤에 '!'를 붙이면 해당 필드에 Non-Null 속성을 적용할 수 있다.
 */
-const typeDefs = gql`
-    type User {
-        id: ID!
-        username: String!
-        firstName: String!
-        lastName: String
-    }
+const typeDefs = gql(`
+  type User {
+    id: ID!
+    username: String!
+    firstName: String!
+    lastName: String
+  }
 
-    type Tweet {
-        id: ID!
-        text: String!
-        author: User!
-    }
+  type Tweet {
+    id: ID!
+    text: String!
+    author: User
+  }
 
-    type Query {
-        allTweets: [Tweet!]!
-        tweet(id: ID!): Tweet
-    }
+  type Query {
+    allTweets: [Tweet!]!
+    tweet(id: ID!): Tweet
+  }
 
-    type Mutation {
-        postTweet(text: String!, userId: ID!): Tweet!
-        deleteTweet(id: ID!): Boolean!
+  type Mutation {
+    postTweet(text: String!, userId: ID!): Tweet!
+    deleteTweet(id: ID!): Boolean!
+  }
+`);
+
+const resolvers = {
+  Query: {
+    allTweets() {
+      return tweets;
+    },
+    tweet(root, { id }) {
+      return tweets.find((tweet) => tweet.id === id);
+    },
+  },
+  Mutation: {
+    postTweet(_, { text, userId }) {
+      const newTweet = {
+        id: tweets.length + 1,
+        text,
+      };
+      tweets.push(newTweet);
+      return newTweet;
+    },
+    deleteTweet(_, { id }) {
+      const tweet = tweets.find((tweet) => tweet.id === id);
+
+      // 매개변수로 받은 id에 해당되는 트윗이 없다면 false 리턴
+      if (!tweet) return false;
+
+      // 있다면 해당하는 트윗을 제외한 트윗만을 담은 배열을 다시 tweets에 입력 후 true 리턴
+      tweets = tweets.filter((tweet) => tweet.id !== id); // 상당히 야매다...
+      return true;
     }
-`;
+  }
+};
 
 // 서버 실행: npm run dev
-
-const server = new ApolloServer({ typeDefs });
+const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen().then(({ url }) => {
-    console.log(`Running on ${url}`);
+  console.log(`Running on ${url}`);
 });
-
