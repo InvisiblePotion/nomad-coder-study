89476bd0a23e87ee98387e9903c0f8a40b09aa13
InvisiblePotion <invisibleppls@gmail.com>
2023-08-26 00:22:51 +0900

    #1.0 완료

--- gulp-for-beginners/gulpfile.babel.js
+++ gulp-for-beginners/gulpfile.babel.js
@@ -1,3 +1,14 @@
-// import gulp from "gulp";
+import gulp from "gulp";
+import gulp_pug from "gulp-pug";
 
-const gulp = require("gulp");
+const routes = {
+    pug: {
+        src: "src/**/*.pug",
+        dest: "build",
+    },
+};
+
+export const pug = () =>
+    gulp.src(routes.pug.src).pipe(gulp_pug()).pipe(gulp.dest(routes.pug.dest));
+
+export const dev = gulp.series([pug]);
