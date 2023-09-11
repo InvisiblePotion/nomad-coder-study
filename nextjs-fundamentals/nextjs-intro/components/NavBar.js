d9faffa934d34c8070e54bb55b249a4b62ff901e
InvisiblePotion <invisibleppls@gmail.com>
2023-09-11 23:59:18 +0900

    #1.5 완료

--- nextjs-fundamentals/nextjs-intro/components/NavBar.js
+++ nextjs-fundamentals/nextjs-intro/components/NavBar.js
@@ -1,36 +1,22 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import styles from "./NavBar.module.css";
 
 export default function NavBar() {
   const router = useRouter();
-  console.log(router);
   return (
-    <nav className={styles.nav}>
-      <Link
-        className={`${styles.link} ${
-          router.pathname === "/" ? styles.active : ""
-        }`}
-        href="/"
-      >
-        Home
-      </Link>
-      <Link
-        className={[
-          styles.link,
-          router.pathname === "/about" ? styles.active : "",
-        ].join(" ")}
-        href="/about"
-      >
-        About
-      </Link>
+    <nav>
+      <Link className={router.pathname === "/" ? "active" : ""} href="/">Home</Link>
+      <Link className={router.pathname === "/about" ? "active" : ""} href="/about">About</Link>
       <style jsx>{`
-      nav {
-        background-color: tomato;
-      }
-      a {
-        text-decoration: none;
-      }
+        nav {
+          background-color: tomato;
+        }
+        a {
+          text-decoration: none;
+        }
+        .active {
+          color: yellow;
+        }
       `}</style>
     </nav>
   );
