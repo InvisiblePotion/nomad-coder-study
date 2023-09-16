643a0ef2d776f920093e5aa29b9cfa732b901fc4
InvisiblePotion <invisibleppls@gmail.com>
2023-09-16 21:24:07 +0900

    #2.0 완료

--- nextjs-fundamentals/nextjs-intro/pages/_app.js
+++ nextjs-fundamentals/nextjs-intro/pages/_app.js
@@ -1,18 +1,16 @@
-import NavBar from "@/components/NavBar";
+import Layout from "@/components/Layout";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <NavBar />
-      <Component {...pageProps} />
-      <style jsx global>{`
-        a {
-          color: black;
-        }
-        .active {
-          /* color: tomato; */
-        }
-      `}</style>
+      <Layout>
+        <Component {...pageProps} />
+        <style jsx global>{`
+          .active {
+            color: tomato;
+          }
+        `}</style>
+      </Layout>
     </>
   );
 }
